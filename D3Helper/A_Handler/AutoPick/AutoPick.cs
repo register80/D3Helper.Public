205c2146dc5031906cb9cf36ca0f3023371a4675
zerenx <zerenx@zxteam.com>
2016-03-28 22:41:54 +0700

    Bugfix for Autopick not recognizing Death s Breath (SNO 449044) as material. Some code cleanup.

--- D3Helper/A_Handler/AutoPick/AutoPick.cs
+++ D3Helper/A_Handler/AutoPick/AutoPick.cs
@@ -229,18 +229,19 @@ namespace D3Helper.A_Handler.AutoPick
 
             public static bool IsGem(ActorCommonData acd)
             {
+                //string ItemName = acd.x004_Name // Not needed but for easier debug
                 switch (acd.x090_ActorSnoId)
                 {
-                    case 437414: //Amethyst_15 Marquise Amethyst
-                    case 437415: //Amethyst_16 Imperial Amethyst
-                    case 437459: //Ruby_15 Marquise Ruby
-                    case 437460: //Ruby_16 Imperial Ruby
-                    case 437481: //x1_Diamond_06 Marquise Diamond
-                    case 437482: //x1_Diamond_07 Imperial Diamond
-                    case 437448: //x1_Emerald_06 Marquise Emerald
-                    case 437449: //x1_Emerald_07 Imperial Emerald
-                    case 437469: //x1_Topaz_06 Marquise Topaz
-                    case 437470: //x1_Topaz_07 Imperial Topaz
+                    case 437414: // Amethyst_15 Marquise Amethyst
+                    case 437415: // Amethyst_16 Imperial Amethyst
+                    case 437459: // Ruby_15 Marquise Ruby
+                    case 437460: // Ruby_16 Imperial Ruby
+                    case 437481: // x1_Diamond_06 Marquise Diamond
+                    case 437482: // x1_Diamond_07 Imperial Diamond
+                    case 437448: // x1_Emerald_06 Marquise Emerald
+                    case 437449: // x1_Emerald_07 Imperial Emerald
+                    case 437469: // x1_Topaz_06 Marquise Topaz
+                    case 437470: // x1_Topaz_07 Imperial Topaz
                         return true;
 
                     default:
@@ -250,31 +251,31 @@ namespace D3Helper.A_Handler.AutoPick
 
             public static bool IsMaterial(ActorCommonData acd)
             {
+                //string ItemName = acd.x004_Name // Not needed but for easier debug
                 switch (acd.x090_ActorSnoId)
                 {
-                    case 361988:    //Crafting_Legendary_05
-                    case 361989:    //Crafting_Looted_Reagent_05
-                    case 361986:    //Crafting_Rare_05
-                    case 361985:    //Crafting_Magic_05
-                    case 361984:    //Crafting_AssortedParts_05
-                    case 137958:    //CraftingMaterials_Flippy_Global
-                    case 365020:    //CraftingReagent_Legendary_Set_Borns_X1
-                    case 364281:    //CraftingReagent_Legendary_Set_Cains_X1
-                    case 364290:    //CraftingReagent_Legendary_Set_Demon_X1
-                    case 364305:    //CraftingReagent_Legendary_Set_Hallowed_X1
-                    case 364975:    //CraftingReagent_Legendary_Set_CaptainCrimsons_X1
-                    case 364725:    //DemonOrgan_Diablo_x1
-                    case 364723:    //DemonOrgan_Ghom_x1
-                    case 364724:    //DemonOrgan_SiegeBreaker_x1
-                    case 364722:    //DemonOrgan_SkeletonKing_x1
-                    case 366949:    //InfernalMachine_Diablo_x1
-                    case 366947:    //InfernalMachine_Ghom_x1
-                    case 366948:    //InfernalMachine_SiegeBreaker_x1
-                    case 366946:    //InfernalMachine_SkeletonKing_x1
-                    case 359504:    //HoradricRelic
-
+                    case 361988:    // Crafting_Legendary_05
+                    case 361989:    // Crafting_Looted_Reagent_05
+                    case 361986:    // Crafting_Rare_05
+                    case 361985:    // Crafting_Magic_05
+                    case 361984:    // Crafting_AssortedParts_05
+                    case 137958:    // CraftingMaterials_Flippy_Global
+                    case 365020:    // CraftingReagent_Legendary_Set_Borns_X1
+                    case 364281:    // CraftingReagent_Legendary_Set_Cains_X1
+                    case 364290:    // CraftingReagent_Legendary_Set_Demon_X1
+                    case 364305:    // CraftingReagent_Legendary_Set_Hallowed_X1
+                    case 364975:    // CraftingReagent_Legendary_Set_CaptainCrimsons_X1
+                    case 364725:    // DemonOrgan_Diablo_x1
+                    case 364723:    // DemonOrgan_Ghom_x1
+                    case 364724:    // DemonOrgan_SiegeBreaker_x1
+                    case 364722:    // DemonOrgan_SkeletonKing_x1
+                    case 366949:    // InfernalMachine_Diablo_x1
+                    case 366947:    // InfernalMachine_Ghom_x1
+                    case 366948:    // InfernalMachine_SiegeBreaker_x1
+                    case 366946:    // InfernalMachine_SkeletonKing_x1
+                    case 359504:    // HoradricRelic
+                    case 449044:    // Deathbreath (itemFlippy_deathsBreath_Flippy_Global)
                         return true;
-
                     default:
                         return false;
                 }
